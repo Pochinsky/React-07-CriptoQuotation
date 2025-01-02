8a2dab48e3edf7d0b95f4caae3f79cfbcd82bd5f
Pochinsky <tomas.guttman@sansano.usm.cl>
2025-01-02 11:58:41 -0300

    🎉 project initializated

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Cotizador de <span>Criptomonedas</span>
+      </h1>
+      <div className="content"></div>
+    </div>
+  );
+}
